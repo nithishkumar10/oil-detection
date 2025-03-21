0631a4d28a9bb644e8aefe514ac850b44b4406fd
NithishKumar <t.nithishkumar9999@gmail.com>
2025-03-21 22:24:27 +0530

    new commit

--- adminapp/admin.py
+++ adminapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . models import Upload_dataset_model,DATASET
+
 
 # Register your models here.
+admin.site.register(Upload_dataset_model)
+admin.site.register(DATASET)
--- userapp/admin.py
+++ userapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from . models import UserModel,Contact_Us,PredictionCount,Predict_details,Last_login,Feedback,PredictionResult
+
+admin.site.register(UserModel)
+admin.site.register(Contact_Us)
+admin.site.register(PredictionCount),
+admin.site.register(Predict_details)
+admin.site.register(Last_login)
+admin.site.register(Feedback)
+admin.site.register(PredictionResult)
